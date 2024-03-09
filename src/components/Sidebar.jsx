b79e9996d23752263844e6939fe03bc595a193dd
Dalton <daltongetbaker@gmail.com>
2024-03-08 23:58:08 -0500

    basic css and starting code

---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Sidebar = () => {
+  return (
+    <>
+    <div className="sidenav sidenav-left">
+
+      </div><div className="sidenav sidenav-right">
+
+          </div>
+          
+          </>
+
+  )
+}
+
+export default Sidebar
---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,14 @@
+// src/pages/HomePage.jsx
+
+import React from "react";
+
+
+function HomePage() {
+  return (
+    <div className="home-page">
+    <div className="page-img"></div>
+    </div>
+  );
+}
+
+export default HomePage;
